f1a3e00a85726a222dca908d47465614b0900642
matheiszr <zsarnok77@gmail.com>
2020-10-15 23:23:26 +0200

    Finish Text start voice
    Finished text commands.
    Start implement voice commands.

--- WahDiscordBot/WahDiscordBot/Modules/TextChannelCommands.cs
+++ WahDiscordBot/WahDiscordBot/Modules/TextChannelCommands.cs
@@ -9,7 +9,10 @@ using Discord.WebSocket;
 
 namespace WahDiscordBot.Modules
 {
-    public class TextBaseCommands : ModuleBase<SocketCommandContext>
+    /// <summary>
+    /// Contains all available commands for the text channels.
+    /// </summary>
+    public class TextChannelCommands : ModuleBase<SocketCommandContext>
     {
         /// For the poll command:
         /// Help: http://www.fileformat.info/search/
@@ -25,29 +28,52 @@ namespace WahDiscordBot.Modules
                         new Emoji("\uD83C\uDDEC"), // g
                     }; // If you need more just add some here...
 
+        /// <summary>
+        /// Write the available command descriptions.
+        /// </summary>
+        /// <returns>Result of the tasc.</returns>
         [Command("help")]
         public async Task Help()
         {
             StringBuilder message = new StringBuilder();
-            message.AppendLine("A bot által felismert parancsok a következők:");
-            message.AppendLine("!help       Kiírja ezt a szöveget.");
-            message.AppendLine("!ping       Egy jó kis ping pong mecs mehet?");
-            message.AppendLine("!echo       Megismétli a beírt szöveget.");
-            message.AppendLine("!poll       Szavazást lehet kiírni vele, melyet a következő formában kell megadni: Kérdés?,válasz1,válasz2,... Legalább 2 válasz lehetőség megadása kötelező és maximum " + _awailableReactions.Count() + " opció lehetséges.");
+            message.AppendLine("- Szöveg csatorna parancsok:");
+            message.AppendLine("**!help** - Kiírja ezt a szöveget.");
+            message.AppendLine("**!ping** - Egy jó kis ping pong mecs mehet?");
+            message.AppendLine("**!echo** - Megismétli a beírt szöveget.");
+            message.AppendLine("**!respects** - Ród le a tiszteletedet valaki előtt az F-el. Paramétere egy személy @név formában.");
+            message.AppendLine("**!wantplay** - Kiírhatsz játék felhívást paramétere egy szöveg, ahol érdemes megadni a játék nevét és időpontját a többiek emottal jelezhetik a részvételt.");
+            message.AppendLine("**!poll** - Szavazást lehet kiírni vele, melyet a következő formában kell megadni: Kérdés?,válasz1,válasz2,... Legalább 2 válasz lehetőség megadása kötelező és maximum " + _awailableReactions.Count() + " opció lehetséges.");
+            /*
+            message.AppendLine("");
+            message.AppendLine("- Hang csatorna parancsok:");
+            message.AppendLine("**!join** - Csatlakozik a paraméterben kapott hang csatornára. Paraméter megadása: #csatornanév");
+            message.AppendLine("**!say** - A paraméter alapján lejátsza a megfelelő voice line-t. További infókért használd a !whatcansay parancsot.");
+            message.AppendLine("**!whatcansay** - Leírja milyne paraméterei lehetnek a say parancsnak.");
+            */
+            var embed = new EmbedBuilder { Color = Color.DarkGrey, Title = "A bot által felismert parancsok a következők:", Description = message.ToString() };
 
-            await ReplyAsync(message.ToString());
+            await ReplyAsync("", false, embed.Build());
         }
 
+        /// <summary>
+        /// Just for test.
+        /// </summary>
+        /// <returns>Result of the tasc.</returns>
         [Command("ping")]
         public async Task Ping()
         {
             await ReplyAsync("pong");
         }
 
+        /// <summary>
+        /// Bot say what you write in parameter.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>Result of the tasc.</returns>
         [Command("echo")]
         public async Task Echo([Remainder] string text)
         {
-            await ReplyAsync(text, true);
+            await ReplyAsync(text);
         }
 
         [Command("respects"), Alias("F")]
@@ -60,6 +86,32 @@ namespace WahDiscordBot.Modules
             await sent.AddReactionAsync(emoji);
         }
 
+        /// <summary>
+        /// Create an embed with a game invite message.
+        /// </summary>
+        /// <param name="message">Contains the game and the time when you want it play.</param>
+        /// <returns>Result of the tasc.</returns>
+        [Command("wantplay")]
+        public async Task WantPlay([Remainder] string message)
+        {
+            if (message == null || message.Trim().Length == 0)
+                return;
+
+            var up = new Emoji("\uD83D\uDC4D");
+            var off = new Emoji("\uD83D\uDC4E");
+            string options = up + " - Jövök!" + Environment.NewLine;
+            options += off + " - Off!";
+
+            var embed = new EmbedBuilder { Color = Color.Blue, Title = message, Description = options };
+            IUserMessage sent = await ReplyAsync("", false, embed.Build());
+            await sent.AddReactionsAsync(new[] { up, off });
+        }
+
+        /// <summary>
+        /// You can create votings with this command.
+        /// </summary>
+        /// <param name="questionAndOptions"></param>
+        /// <returns>Result of the tasc.</returns>
         [Command("poll")]
         public async Task Poll([Remainder] string questionAndOptions)
         {
@@ -75,8 +127,6 @@ namespace WahDiscordBot.Modules
                 {
                     string question = parts[0];
                     string[] answerOptions = parts.ToList().GetRange(1, parts.Length-1).ToArray();
-
-                    
 
                     List<Emoji> usedEmotes = new List<Emoji>();
 
---
+++ WahDiscordBot/WahDiscordBot/Modules/VoiceChannelCommands.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace WahDiscordBot.Modules
+{
+    public class VoiceChannelCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("join", RunMode = RunMode.Async)]
+        public async Task JoinVoiceChannel(IVoiceChannel channel = null)
+        {
+            try
+            {
+                channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+                if (channel == null)
+                {
+                    await Context.Channel.SendMessageAsync("Vagy legyél fent hangoson, ha már hívsz vagy add meg hova menjek, ennyit se tudsz?");
+                    return;
+                }
+                var audioClient = await channel.ConnectAsync();
+            } 
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+        }
+    }
+}
